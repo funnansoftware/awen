dd332f7b67c2e46cf60526339328cf471b6e2048
funnan-dev <funnan@funnansoftware.com>
2026-05-15 20:38:47 -0400

    Reset the repo.

--- src/awen/core/test/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-#include <QCoreApplication>
-
-auto main(int argc, char* argv[]) -> int
-{
-    QCoreApplication app(argc, argv);
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
